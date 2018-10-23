0191b0b13e951580dc5021d789d66c6b34483264
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 17:21:54 +0200

    Add RTS Project

---
+++ Real-Time-Strategy/Assets/RTS/Scripts/Selectable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selectable : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "RTS Selection") {
+			GetComponent<Renderer>().material.color = Color.green;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.name == "RTS Selection") {
+			GetComponent<Renderer>().material.color = Color.red;
+		}
+	}
+
+}
